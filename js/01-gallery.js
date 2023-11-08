8d9d6bdd5fbcd7560872293427bf6e23cec42bde
Dawid Jasiński <ambas90@gmail.com>
2023-11-08 22:54:28 +0100

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+let instance;
+// tworzenie elementów galerii
+const markup = galleryItems
+  .map(
+    (image) => `<li>
+<div class="gallery__item">
+  <a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>
+</li>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+//otwieranie podglądu obrazu
+
+function handleClick(event) {
+  event.preventDefault();
+  const selectedImage = event.target.dataset.source;
+  instance = basicLightbox.create(
+    `
+  		<img width="1400" height="900" src="${selectedImage}">
+  	`
+  );
+  instance.show();
+
+  if (instance.visible()) {
+    document.addEventListener("keydown", closePreview);
+  } else {
+    document.removeEventListener("keydonw", closePreview);
+  }
+}
+//zamykanie podglądu obrazu z klawisza Esc
+function closePreview(event) {
+  if (event.key == "Escape") {
+    instance.close();
+  }
+}
+
+gallery.addEventListener("click", handleClick);
