9450f569e4a51cfbe20a03037a99495f7a317579
Dawid Jasiński <ambas90@gmail.com>
2023-11-08 23:24:41 +0100

    update task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+// tworzenie elementów galerii
+const markup = galleryItems
+  .map(
+    (image) => `<a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>
+</li>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("afterbegin", markup);
+
+const galleryImages = document.querySelectorAll(".gallery__item");
+
+new SimpleLightbox(galleryImages, {
+  captionData: "alt",
+  captionDelay: 250,
+  captionPosition: "bottom",
+});
